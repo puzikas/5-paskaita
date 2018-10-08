08a8acd6286431ba140fd32a3f0d3c30d0f39c0c
Lukas <lukas.chrap@gmail.com>
2018-10-08 21:26:36 +0300

    10.08

---
+++ app.js
@@ -0,0 +1,22 @@
+// var langeliai = document.getElementById('langeliai')
+
+// for (var i = 0; i <= 100; i += 5) {
+// 	langeliai.innerHTML += '<div class="col logo">'+i+'</div>';
+// }
+
+function generate() {
+
+	var langeliai = document.getElementById('langeliai');
+
+	langeliai.innerHTML = '';
+
+	for (var i = 0; i < 101; i += 5) {
+		console.log(i);
+		var r = Math.round(Math.random() * 255);
+		var g = Math.round(Math.random() * 255);
+		var b = Math.round(Math.random() * 255);
+		langeliai.innerHTML += '<div class="langeliai" style="background-color: rgb('+r+','+g+','+b+')">' + i + '</div>';
+
+	}	
+}
+
